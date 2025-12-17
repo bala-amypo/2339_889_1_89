5143983cc79dedb5eb398ef3e7ad1ae63f0fcf6e
bala-amypo <bala@amypo.in>
2025-12-17 08:48:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileService.java
@@ -0,0 +1,8 @@
+package com.example.demo.newservice;
+
+import com.example.demo.newentity.StudentValidation;
+
+public interface NewfileService{
+    StudentValidation savedata(StudentValidation newfile);
+    StudentValidation getidval(Long id);
+}
