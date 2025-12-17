194b13c38b22989c931e3891321757deb5fbc479
bala-amypo <bala@amypo.in>
2025-12-17 09:17:18 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileserviceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileserviceImpl.java
@@ -2,5 +2,13 @@ package com.example.service.impl;
 import java.util.*;
 public class NewfileServiceImpl implements StudentService{
     @override
-    public StudentValidation
+    public StudentValidation savedata(StudentEntity st){
+
+    }
+    @override
+    public StudentValidation getidval(Long id){
+
+    }
+    @override
+    public List<StudentEntity> getall(){}
 }
