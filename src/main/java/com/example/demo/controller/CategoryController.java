566f679044b9a61a09fa78a5b1b6a7b0cb8ebf7c
bala-amypo <bala@amypo.in>
2025-12-18 19:02:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import com.example.demo.entity.Category;
+import com.example.demo.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/Categories")
+@Tag(name = "Categories Endpoints")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category){
+        return categoryService.createCategory(category);
+    }
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable Long id){
+        return categoryService.getCategory(id);
+    }
+}
