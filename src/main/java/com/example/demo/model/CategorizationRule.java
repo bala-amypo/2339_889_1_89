ef95d2c3550334c103bbae3398ee555150359c9a
bala-amypo <bala@amypo.in>
2025-12-18 10:12:37 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,5 +1,43 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constranints.*;
-@StudentEntity
+@CategorizationRule
 @Table(name="Student")
+
+public class CategorizationRule(){
+    private Long id;
+    private Category category;
+    private String Keyword;
+    private Integer priority;
+    private LocalDateTime createdAt;
+    
+    public CategorizationRule(){
+
+    }
+     public Category(Long id,Category category,String Keyword,Integer priority,LocalDateTime createdAt){
+        this.id=id;
+        this.categoryName=categoryName;
+        this. description=description;
+        this.createdAt=createdAt;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getCategoryName(){
+        return categoryName;
+    }
+    public void setCategoryName(String CategoryName){
+        this.categoryName=categoryName;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+}
+
