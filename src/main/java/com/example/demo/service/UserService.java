0c092f599b27fb78d2ce57387f515a9a8855b639
bala-amypo <bala@amypo.in>
2025-12-18 10:37:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.ApiKeyEntity;
+
+public interface ApiKeyService {
+
+    ApiKeyEntity create(ApiKeyEntity apiKey);
+
+    ApiKeyEntity update(Long id, ApiKeyEntity apiKey);
+
+    ApiKeyEntity getById(Long id);
+
+    List<ApiKeyEntity> getAll();
+
+    void deactivate(Long id);
+}
