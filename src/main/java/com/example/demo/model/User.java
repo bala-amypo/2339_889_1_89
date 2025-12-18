bf14fa35a5a54512341719703340a391e556ba6e
bala-amypo <bala@amypo.in>
2025-12-18 09:31:25 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -11,18 +11,38 @@ public class User(){
     private String password;
     private String ADMIN;
     private String USER;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     public User(){
 
     }
-    public User(Long id,String fullName,String email,String password,String ADMIN,String USER,LocalDateTime createAt){
+    public User(Long id,String fullName,String email,String password,String ADMIN,String USER,LocalDateTime createdAt){
         this.id=id;
-    private String fullName;
-    private String email;
-    private String password;
-    private String ADMIN;
-    private String USER;
-    private LocalDateTime createAt;
+        this.fullName=fullname;
+        this.email=email;
+        this.password=password;
+        this.ADMIN=ADMIN;
+        this.USER=USER;
+        this.createAt=createdAt;
     }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long Id){
+        this.id=id;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String FullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String Email){
+        this.email=email;
+    }
+    
 }
