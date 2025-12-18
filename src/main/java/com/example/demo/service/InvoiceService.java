bf84ce8ba0a3d2f4d2b187c12a4f57dbca91b0e6
bala-amypo <bala@amypo.in>
2025-12-18 17:06:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/InvoiceService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.entity.Invoice;
+
+public interface InvoiceService{
+    Invoice unloadInvoice(long userId,Long vendorId,Invoice invoice);
+    Invoice Categorized
+}
