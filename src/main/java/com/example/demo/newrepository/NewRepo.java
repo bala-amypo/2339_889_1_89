c7b915b43a17a77c8284f224a52067fcb6afbf26
bala-amypo <bala@amypo.in>
2025-12-18 06:27:50 +0000

    User commit

--- src/main/java/com/example/demo/newrepository/NewRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.newrepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.newentity.StudentValidation;
-
-public interface NewRepo extends JpaRepository<NewRepo,Long>{
-
-}
