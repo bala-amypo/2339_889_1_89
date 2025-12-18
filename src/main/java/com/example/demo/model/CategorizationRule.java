8522408e7b1b9c3791ed920caadf015c16388a31
bala-amypo <bala@amypo.in>
2025-12-18 06:54:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constranints.*;
+@StudentEntity
+@Table(name="Student")
---
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constranints.*;
+@StudentEntity
+@Table(name="Student")
+
+public class 
