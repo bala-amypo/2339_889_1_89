f7bad02b0a8ef56f524dff221d596e4bb1e18b6e
bala-amypo <bala@amypo.in>
2025-12-18 08:06:53 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -24,5 +24,8 @@ public class Vendor{
     public Long getId(){
         return id;
     }
-    public void setId(Long Id)
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getvendorName()
 }
