5b0915d5535d9d94ddf97587446feb714e911a4a
bala-amypo <bala@amypo.in>
2025-12-19 06:19:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.CategorizationRuleRepository;
+import com.example.demo.repository.CategorizationRuleRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategorizationRuleRepository extends JpaRepository<CategorizationRuleRepository, Long> {
--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.InvoiceRepository;
+import com.example.demo.repository.InvoiceRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<InvoiceRepository, Long> {
