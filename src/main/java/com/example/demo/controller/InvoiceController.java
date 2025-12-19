cfc99632ac9fe6c9d706cebb1be7323b0adf0931
bala-amypo <bala@amypo.in>
2025-12-19 03:54:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/InvoiceController.java
+++ src/main/java/com/example/demo/controller/InvoiceController.java
@@ -7,5 +7,16 @@ import com.example.demo.entity.Invoice;
 import com.example.demo.service.InvoiceService;
 
 @RestController
-@RequestMapping("/api/invoices");
-@Tag(name = "Categories Endpoints")
+@RequestMapping("/api/invoices")
+@Tag(name = "Invoice Endpoints")
+public class InvoiceController{
+    @Autowired
+    private InvoiceService invoiceService ;
+
+    @PostMapping("/upload/{userId}/{vendorId}")
+    public Invoice uploadInvoice{
+        @pathVariable Long userId;
+        @pathVariable Long vendor
+    }
+
+}
