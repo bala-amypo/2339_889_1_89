834b999a87878c33d6c52a14f8da7f510d94424d
bala-amypo <bala@amypo.in>
2025-12-19 04:52:17 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constranints.*;
+import java.time.LocalDateTime
 @CategorizationRule
 @Table(name="Student")
 
