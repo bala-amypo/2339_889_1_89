0874b19cb4ed3da87c4f4a8a2a5aeeabaae9f067
bala-amypo <bala@amypo.in>
2025-12-20 16:45:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
@@ -1,38 +1,3 @@
-package com.example.demo.service.impl;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.model.CategorizationRule;
-import com.example.demo.repository.CategorizationRuleRepository;
-import com.example.demo.service.CategorizationRuleService;
-
-@Service
-public class CategorizationRuleServiceImpl implements CategorizationRuleService {
-    
-    @Autowired
-    private CategorizationRuleRepository rep;
-    public 
-    @Override CategorizationRuleServiceImpl (CategorizationRuleRepository rep){
-        this.rep=rep;
-    }
-    public CategorizationRule createRule(Long categoryId,CategorizationRule rule) {
-        rule.setCategory(categoryRepository.findById(categoryId).orElse(null));
-        return rep.save(rule);
-    }
-
-    @Override
-    public List<CategorizationRule> getRulesByCategory(Long categoryId){
-        return rep.findByCategoryId(categoryId);
-    }
-
-    @Override
-    public void deleteRule(Long ruleId){
-        rep.deleteById(ruleId);
-    }
-}
 
 package com.example.demo.service.impl;
 
