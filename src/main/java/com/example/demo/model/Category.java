48bda0d8e9b23f608cae2d278fcc52431aa5ac7f
bala-amypo <bala@amypo.in>
2025-12-21 11:15:42 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,50 +1,58 @@
 
 package com.example.demo.model;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import java.time.LocalDateTime;
 import java.util.*;
-
 @Entity
-@Table(name="categories")
-public class Category {
+@Table(name="Student")
+
+public class Category{
     @Id
+    
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique=true)
     private String categoryName;
-
     private String description;
     
     private LocalDateTime createdAt;
     
-    // This tells JPA that the 'category' field in the User class owns the relationship
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category")
     private List<User> users;
+    public Category(){
 
-    public Category() {}
-
-    public Category(Long id, String categoryName, String description, LocalDateTime createdAt) {
-        this.id = id;
-        this.categoryName = categoryName;
-        this.description = description;
-        this.createdAt = createdAt;
+    }
+    public Category(Long id,String categoryName,String description,LocalDateTime createdAt){
+        this.id=id;
+        this.categoryName=categoryName;
+        this.description=description;
+        this.createdAt=createdAt;
     }
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getCategoryName(){
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName){
+        this.categoryName=categoryName;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt( LocalDateTime getCreatedAt){
+        this.createdAt=createdAt;
+    }
 
-    public String getCategoryName() { return categoryName; }
-    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
-
-    public List<User> getUsers() { return users; }
-    public void setUsers(List<User> users) { this.users = users; }
 }
