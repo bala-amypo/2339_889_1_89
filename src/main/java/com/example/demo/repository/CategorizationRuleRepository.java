3a64ad896c0816155b6809bdfef9de8ed9286440
bala-amypo <bala@amypo.in>
2025-12-21 11:52:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.CategorizationRule;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
+public interface CategorizationRuleRepository
+        extends JpaRepository<CategorizationRule, Long> {
+
     List<CategorizationRule> findMatchingRulesByDescription(String description);
 }
