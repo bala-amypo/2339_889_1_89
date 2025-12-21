ea17ba605473c30ef1564195349aa7455ed38c8a
bala-amypo <bala@amypo.in>
2025-12-21 11:52:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Invoice;
 import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
     List<Invoice> findByUploadedBy(User user);
+
     List<Invoice> findByAmountGreaterThanHql(Double amount);
 }
