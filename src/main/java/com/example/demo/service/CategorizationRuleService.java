4b5798a195faa33ed1c8fa747d80d05d662082dd
bala-amypo <bala@amypo.in>
2025-12-21 11:57:39 +0000

    User commit

--- src/main/java/com/example/demo/service/CategorizationRuleService.java
+++ src/main/java/com/example/demo/service/CategorizationRuleService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service;
-import java.util.List;
-import com.example.demo.model.CategorizationRule;
 
-public interface CategorizationRuleService{
-    CategorizationRule createRule(Long categoryId,CategorizationRule rule);
+import com.example.demo.model.CategorizationRule;
+import java.util.List;
+
+public interface CategorizationRuleService {
+
+    CategorizationRule createRule(Long categoryId, CategorizationRule rule);
+
     List<CategorizationRule> getRulesByCategory(Long categoryId);
+
     void deleteRule(Long ruleId);
 }
