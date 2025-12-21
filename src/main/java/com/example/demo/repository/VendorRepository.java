a4f0edcd976b90c9369d4a6881963ee9a46a0add
bala-amypo <bala@amypo.in>
2025-12-21 09:23:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.repository.VendorRepository;
+import com.example.demo.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VendorRepository extends JpaRepository<VendorRepository, Long> {
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
 
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.impl;
-import java.util.*;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.User;
@@ -22,8 +22,7 @@ public class UserServiceImpl implements UserService{
         return userRepository.findAll();
     }
     @Override
-    public User findByEmail(String email){
-        Optional<User> user = userRepository.findByEmail(email);
-        return user;
+    public <Optonal<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
     }
 }
