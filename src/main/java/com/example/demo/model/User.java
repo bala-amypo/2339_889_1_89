78dd72983df440d9f8d80b56492a206acd5dfda2
bala-amypo <bala@amypo.in>
2025-12-22 07:57:47 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -17,7 +17,7 @@ public class User {
     private String role;
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    // ✅ GETTERS & SETTERS
+    
     public Long getId() { return id; }
     public String getEmail() { return email; }
     public void setRole(String role) { this.role = role; }
