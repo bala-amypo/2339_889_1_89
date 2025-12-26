4b3b9186fdff76c3b47d6dcb83f38e31e1160d8c
bala-amypo <bala@amypo.in>
2025-12-26 15:12:10 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,34 +1,66 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
+@Table(name = "vendors")
 public class Vendor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotBlank @Column(unique = true)
     private String vendorName;
+
+    @Email @NotBlank
+    private String contactEmail;
+
+    private String address;
+
     private LocalDateTime createdAt;
-    
     @ManyToMany(mappedBy = "favoriteVendors")
     private Set<User> users = new HashSet<>();
-    
+
+  
+    @OneToMany(mappedBy = "vendor")
+    private List<Invoice> invoices;
+
+    public Vendor() {}
+
+    public Vendor(String vendorName, String contactEmail, String address,Set<User> users) {
+        this.vendorName = vendorName;
+        this.contactEmail = contactEmail;
+        this.address = address;
+        this.users=users;
+    }
+
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
     }
-    
-    // Getters and setters
+
+    public Set<User> getUsers() {
+        return users;
+    }
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
     public String getVendorName() { return vendorName; }
     public void setVendorName(String vendorName) { this.vendorName = vendorName; }
+
+    public String getContactEmail() { return contactEmail; }
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
-    public Set<User> getUsers() { return users; }
-    public void setUsers(Set<User> users) { this.users = users; }
+
+    public List<Invoice> getInvoices() { return invoices; }
+    public void setInvoices(List<Invoice> invoices) { this.invoices = invoices; }
 }
