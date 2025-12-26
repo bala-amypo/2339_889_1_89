c14ff8d03208c104bde380aeccde7824452a817c
bala-amypo <bala@amypo.in>
2025-12-26 17:32:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -25,7 +25,7 @@ public class Vendor {
         this.createdAt = LocalDateTime.now();
     }
     
-    // Getters and setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
