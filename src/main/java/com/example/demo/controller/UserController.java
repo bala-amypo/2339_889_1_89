319933caaa32f5cafd907a00309f7258a1397229
bala-amypo <bala@amypo.in>
2025-12-26 08:13:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,31 +1,28 @@
 package com.example.demo.controller;
-import java.util.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.User;
+import com.example.demo.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@Tag(name = "Users Endpoints")
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserServiceImpl userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user){
-        return userService.registerUser(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
     }
-    @GetMapping("/all")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-    @GetMapping("/email/{email}")
-    Optional<User>findByEmail(@PathVariable String email) {
-        return userService.findByEmail(email);
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody User loginRequest) {
+        AuthResponse response = new AuthResponse("mock-token", 1L, loginRequest.getEmail(), "USER");
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -1,32 +1,21 @@
 package com.example.demo.controller;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import com.example.demo.model.Vendor;
-import com.example.demo.service.VendorService;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.model.Vendor;
+import com.example.demo.service.impl.VendorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/vendors")
-@Tag(name = "Vendors Endpoints")
 public class VendorController {
 
     @Autowired
-    VendorService vendorService;
+    private VendorServiceImpl vendorService;
 
     @PostMapping
-    public Vendor createVendor(@RequestBody Vendor vendor){
-        return vendorService.createVendor(vendor);
-    }
-    @GetMapping
-    public List<Vendor> getAllVendors(){
-        return vendorService.getAllVendors();
-    }
-
-    @GetMapping("/{vendorId}")
-    public Vendor getVendor(@PathVariable Long vendorId){
-        return vendorService.getVendor(vendorId);
+    public ResponseEntity<Vendor> createVendor(@RequestBody Vendor vendor) {
+        Vendor savedVendor = vendorService.createVendor(vendor);
+        return ResponseEntity.ok(savedVendor);
     }
 }
