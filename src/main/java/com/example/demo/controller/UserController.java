dd8e907aa742162de34a5589f0913a0c08a196eb
bala-amypo <bala@amypo.in>
2025-12-26 15:50:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,29 +1,22 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.example.demo.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@Tag(name = "Users")
+@CrossOrigin(origins = "*")
 public class UserController {
-
-    private final UserService userService;
-
-    public UserController(UserService userService) { this.userService = userService; }
-
-    @GetMapping("/all")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @GetMapping("/{email}")
-    public User getUserByEmail(@PathVariable String email) {
-        return userService.findByEmail(email);
+    
+    @Autowired
+    private UserServiceImpl userService;
+    
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
     }
 }
