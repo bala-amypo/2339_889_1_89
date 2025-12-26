b15e74e43b18596f5538a515520488d9cbe7a8ef
bala-amypo <bala@amypo.in>
2025-12-26 15:03:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/InvoiceUploadRequest.java
@@ -0,0 +1,46 @@
+package com.example.demo.dto;
+
+import java.time.LocalDate;
+
+public class InvoiceUploadRequest {
+
+    private String invoiceNumber;
+    private Double amount;
+    private LocalDate invoiceDate;
+    private String description;
+
+    public InvoiceUploadRequest() {
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(LocalDate invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
