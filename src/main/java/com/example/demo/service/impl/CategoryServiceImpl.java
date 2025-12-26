ff6c391abb704af3c3d2703ba37199b764749ed5
bala-amypo <bala@amypo.in>
2025-12-26 15:19:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -2,25 +2,37 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
-import com.example.demo.service.CategoryService;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.service.CategoryService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
+@Transactional
 public class CategoryServiceImpl implements CategoryService {
+
     private final CategoryRepository categoryRepository;
-    
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
     }
-    
+
     @Override
-    public Category createCategory(Category category) {
+    public Category createCategory(Category category){
+        if(categoryRepository.existsByCategoryName(category.getCategoryName()))
+            throw new RuntimeException("Category name already exists");
         return categoryRepository.save(category);
     }
-    
-    public Category getCategory(Long id) {
-        return categoryRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+
+    @Override
+    public Category getCategory(Long categoryId){
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    }
+
+    @Override
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
     }
 }
