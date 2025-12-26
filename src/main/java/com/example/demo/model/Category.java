be59eb539c460b7b68f00e0ea08bb14336b2bdfd
bala-amypo <bala@amypo.in>
2025-12-26 17:30:44 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -20,7 +20,7 @@ public class Category {
         this.createdAt = LocalDateTime.now();
     }
     
-    // Getters and setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
