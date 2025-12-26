81543ddda100d45c7f7150ce3f997cf8b38e5a64
bala-amypo <bala@amypo.in>
2025-12-26 15:14:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Category;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository {
-    Category save(Category category);
-    Optional<Category> findById(Long id);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByCategoryName(String categoryName);
 }
